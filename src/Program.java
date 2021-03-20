4f418a773226144932bc776b12894dff516904a6
tinaklsnk <valentaine29012002@gmail.com>
2021-03-20 19:28:26 +0200

    finished

--- src/Program.java
+++ src/Program.java
@@ -4,18 +4,11 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.*;
-
-import java.io.File;
 import org.w3c.dom.*;
 
 public class Program {
@@ -27,59 +20,34 @@ public class Program {
             Document doc = dBuilder.parse(inputFile);
             Document newdoc = dBuilder.newDocument();
             doc.getDocumentElement().normalize();
-
+            NodeList wpt = doc.getDocumentElement().getElementsByTagName("wpt");
             NodeList desc = doc.getDocumentElement().getElementsByTagName("desc");
             NodeList link = doc.getDocumentElement().getElementsByTagName("link");
-            NodeList wpt = doc.getDocumentElement().getElementsByTagName("wpt");
             String lon, lat, text, attribute;
             Element kml = newdoc.createElement("kml");
-            newdoc.appendChild(kml);
             Element document = newdoc.createElement("Document");
+            newdoc.appendChild(kml);
             kml.appendChild(document);
-            //int listlenght = nDesc.getLength();
-           // NodeList nList = doc.getDocumentElement().getChildNodes();
-            for (int i = 0; i < desc.getLength();  i++) {
-                /*
-                Node nNode = nDesc.item(i);
-                text = nNode.getTextContent();
-                 */
+            for (int i = 0; i < wpt.getLength();  i++) {
                 text = desc.item(i).getTextContent();
                 attribute = link.item(i).getAttributes().item(0).getTextContent();
-/*
-                Node link = nLink.item(i);
-                NamedNodeMap attributes = link.getAttributes();
-                Node attribute = attributes.item(0);
-                decription = attribute.getTextContent();
-                decription = nLink.item(i).getAttributes().item(0).getTextContent();
-                Node wpt = nWpt.item(i);
-                NamedNodeMap wptattributes = wpt.getAttributes();
-                Node wptLat = wptattributes.item(0);
-                Node wptLon = wptattributes.item(1);
-                lat = wptLat.getTextContent();
-                lon = wptLon.getTextContent();
-                 */
                 lat = wpt.item(i).getAttributes().item(0).getTextContent();
                 lon = wpt.item(i).getAttributes().item(1).getTextContent();
-
                 Element placemark = newdoc.createElement("Placemark");
-                document.appendChild(placemark);
-
                 Element name = newdoc.createElement("name");
+                document.appendChild(placemark);
                 placemark.appendChild(name);
                 if (text != null) {
                     name.appendChild(newdoc.createTextNode(text));
                 }
-
                 Element descript = newdoc.createElement("description");
                 placemark.appendChild(descript);
                 if (attribute != null) {
                     descript.appendChild(newdoc.createTextNode(attribute));
                 }
-
                 Element point = newdoc.createElement("Point");
-                placemark.appendChild(point);
-
                 Element coord = newdoc.createElement("coordinates");
+                placemark.appendChild(point);
                 point.appendChild(coord);
                 if (lat != null && lon != null) {
                     Text textNode = newdoc.createTextNode(lat + "," + lon);
